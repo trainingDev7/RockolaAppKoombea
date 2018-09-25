e78a0edd6206df7be56197cd2b7f18f5bce44274
Felipe Llerena <felipe.llerena@koombea.com>
2018-09-25 10:09:43 -0500

    add sign_in for koombea users

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+
+  def self.find_from_google_oauth2(auth_data)
+    where(email: auth_data.email).first_or_create!({
+      password: Devise.friendly_token[0, 20],
+      name: "#{auth_data.first_name} #{auth_data.last_name}"
+    })
+  end
+
+end
