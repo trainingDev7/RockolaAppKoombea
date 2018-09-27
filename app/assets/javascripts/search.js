874d2107d5f78bec8ceca23afd0152b1a2b17b78
Felipe Llerena <felipe.llerena@koombea.com>
2018-09-27 09:04:51 -0500

    Add delete permissions and pagination search

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -68,28 +68,3 @@ app.Rockola.onSearchResponse = function(response) {
       }
     });
   });
-
-  var player;
-  function onYouTubePlayerAPIReady() {
-      player = new YT.Player('player', {
-        width: '640',
-        height: '390',
-        videoId: '0Bmhjf0rKe8',
-        events: {
-          onReady: onPlayerReady,
-          onStateChange: onPlayerStateChange
-        }
-      });
-  }
-
-  // autoplay video
-  function onPlayerReady(event) {
-      event.target.playVideo();
-  }
-
-  // when video ends
-  function onPlayerStateChange(event) {
-      if(event.data === 0) {
-          alert('done');
-      }
-  }
