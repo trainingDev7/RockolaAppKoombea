05361099d52b896f831f6e5618ff194d499b55ef
Luisma <luis.silvera@koombea.com>
2018-09-28 11:27:07 -0500

    Refactoring JS and HTML

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,27 +1,35 @@
+var app = app || {};
+app.Rockola = app.Rockola || {};
+
 function onClientLoad() {
-    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
-  }
+  gapi.client.load('youtube', 'v3', app.Rockola.onYouTubeApiLoad);
+  $('#onSearchButton').on('click', app.Rockola.search);
+  $('#navbarTogglerDemo01 > form').on('submit', function(e){ e.preventDefault(); });
+}
 
-  function onYouTubeApiLoad() {
-    gapi.client.setApiKey('AIzaSyAWhX3xLCfnJ-U0HEPo7R0Zc-5J9jCJoXc');
-  }
+app.Rockola.onYouTubeApiLoad = function() {
+  gapi.client.setApiKey('AIzaSyAWhX3xLCfnJ-U0HEPo7R0Zc-5J9jCJoXc');
+}
 
-  function search() {
-    $('#navbarTogglerDemo01 > form').on('submit', function(e){ e.preventDefault(); });
-    var query = document.getElementById('query').value;
-    var request = gapi.client.youtube.search.list({
-      part: 'snippet',
-      type: 'video',
-      maxResults: '10',
-      q:query
-    });
-    request.execute(onSearchResponse);
-  }
+app.Rockola.search = function() {
+  var query = $('#query').val();
+  var request = gapi.client.youtube.search.list({
+    part: 'snippet',
+    type: 'video',
+    maxResults: '10',
+    q:query
+  });
+  request.execute(app.Rockola.onSearchResponse);
+}
 
-  function onSearchResponse(response) {
-      $("#response").html('');
-      response.items.forEach(function(e){
-          $("#response").append('<img width="'+ e.snippet.thumbnails.medium.width + '"src="'+ e.snippet.thumbnails.medium.url +'" height="'+
-               e.snippet.thumbnails.medium.height +'" value="' + e.snippet.title + '">' + e.snippet.title + '<br></br>');
-      });
-  }
+app.Rockola.onSearchResponse = function(response) {
+  $("#response").html('');
+  response.items.forEach(function(e){
+    $("#response").append('<img width="'+ e.snippet.thumbnails.medium.width +
+                          '"src="'+ e.snippet.thumbnails.medium.url +
+                          '" height="'+ e.snippet.thumbnails.medium.height +
+                          '" value="' + e.snippet.title +
+                          '">' + e.snippet.title +
+                          '<br></br>');
+  });
+}
