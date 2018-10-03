19872999d3c3ea115565be0944ef7e8e9c7a58f2
Luisma <luis.silvera@koombea.com>
2018-10-03 15:32:31 -0500

    Refactoring JS and HTML

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -1,32 +1,29 @@
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.ENDED){
-      // your code to run after video has ended
-      player.current_video++;
-      playVideo()
+var app = app || {};
+app.Rockola = app.Rockola || {};
+app.Rockola.Player = app.Rockola.Player || {};
+
+app.Rockola.Player.player = '';
+
+app.Rockola.Player.onPlayerStateChange = function(event) {
+  if (event.data == YT.PlayerState.ENDED){
+    app.Rockola.Player.player.current_video++;
+    app.Rockola.Player.playVideo()
+  }
+}
+
+app.Rockola.Player.playVideo = function() {
+  var videoId = $('#js-playlist-content li[data-video]').eq(app.Rockola.Player.player.current_video).attr('data-video');
+  app.Rockola.Player.player.loadVideoById(videoId, "large");
+}
+
+app.Rockola.Player.onYouTubeIframeAPIReady = function() {
+  app.Rockola.Player.player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: '',
+    events: {
+      'onStateChange': app.Rockola.Player.onPlayerStateChange
     }
-  }
-  
-  var player = document.querySelector('iframe');
-  function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-      height: '390',
-      width: '640',
-      videoId: '',
-      events: {
-        'onStateChange': onPlayerStateChange
-      }
-    });
-    
-    // Create a property on the player object to keep track of the current video index
-    player.current_video = 0;
-  }
-  
-  $(document).on('click', '[data-video]', function(){
-    player.current_video = $(this).index();
-    playVideo();
-  })
-  
-  function playVideo(){
-    var video_id = $('[data-video]').eq(player.current_video).attr('data-video');
-    player.loadVideoById(video_id, "large")
-  }
+  });
+  app.Rockola.Player.player.current_video = 0;
+}
--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,104 +1,91 @@
 var app = app || {};
 app.Rockola = app.Rockola || {};
 
-  function onClientLoad() {
+function onClientLoad() {
   gapi.client.load('youtube', 'v3', app.Rockola.onYouTubeApiLoad);
   $('#onSearchButton').on('click', app.Rockola.search);
   $('#navbarTogglerDemo01 > form').on('submit', function(e){ e.preventDefault(); });
+  $('#next').on('click', function(e) {
+    e.preventDefault();
+    app.Rockola.search(app.Rockola.tokenNextPage);
+  });
+  app.Rockola.Player.onYouTubeIframeAPIReady();
+
+  $(document).on('click', '#js-playlist-content li[data-video]', function(){
+    app.Rockola.Player.player.loadVideoById($(this).data('video'), "large");
+  })
 }
+
+app.Rockola.tokenNextPage = '';
 
 app.Rockola.onYouTubeApiLoad = function() {
   gapi.client.setApiKey('AIzaSyAWhX3xLCfnJ-U0HEPo7R0Zc-5J9jCJoXc');
-}
+};
 
-app.Rockola.search = function() {
+app.Rockola.search = function(token) {
   var query = $('#query').val();
-  var request = gapi.client.youtube.search.list({
+  var optionsQuery = {
     part: 'snippet, id',
     type: 'video',
     maxResults: '10',
-    q:query
-  });
+    q: query
+  };
+
+  var request = gapi.client.youtube.search.list( token.length > 0 ? $.extend(optionsQuery, {pageToken: token}) : optionsQuery);
   request.execute(app.Rockola.onSearchResponse);
-}
+};
 
 app.Rockola.onSearchResponse = function(response) {
-  var nextToken = response.nextPageToken;
+  app.Rockola.tokenNextPage = response.nextPageToken;
+
   $("#response").html('');
-    nextButton(nextToken);
+
   response.items.forEach(function(e){
     $("#response").append('<img data-toggle="modal" data-target="#videoModal" class="video" rel="https://www.youtube.com/embed/'+ e.id.videoId +
-                          '"src="'+ e.snippet.thumbnails.medium.url +
-                          '" value="' + e.snippet.title +
-                          '"><br></br>');
+                      '"src="'+ e.snippet.thumbnails.medium.url +
+                      '" value="' + e.snippet.title +
+                      '"><br></br>');
   });
-}
 
- app.Rockola.nextPage = function(token) {
-  var query = $('#query').val();
-  var request = gapi.client.youtube.search.list({
-    part: 'snippet, id',
-    type: 'video',
-    maxResults: '10',
-    q:query,
-    pageToken: token
-  });
-  request.execute(app.Rockola.onSearchResponse);
-}
-
-app.Rockola.onSearchResponse = function(response) {
-  var nextToken = response.nextPageToken;
-  $("#response").html('');
-      nextButton(nextToken);
-      console.log(nextToken);
-  response.items.forEach(function(e){
-    $("#response").append('<img data-toggle="modal" data-target="#videoModal" class="video" rel="https://www.youtube.com/embed/'+ e.id.videoId +
-                          '"src="'+ e.snippet.thumbnails.medium.url +
-                          '" value="' + e.snippet.title +
-                          '"><br></br>');
-  });
   $("#next").show();
-}
+};
 
-  $(function(){
-
+$(function(){
   $("#next").hide();
 
   $('#videoModal').on('shown.bs.modal', function (e) {
     var videoSRC = $(e.relatedTarget).attr("rel")
-    $('#videoModal iframe').attr('src', videoSRC + "?autoplay=1&showinfo=0");
     var valVideo = $(e.relatedTarget).attr("value");
+    var songId = videoSRC.split("https://www.youtube.com/embed/")
+
+    $('#videoModal iframe').attr('src', videoSRC + "?autoplay=1&showinfo=0");
     $('.modal-title').append('<h4>' + valVideo + '</h4>');
     $("#song_title").val(valVideo);
-    var songId = videoSRC.split("https://www.youtube.com/embed/")
     $("#song_videoId").val(songId[1])
-  })
+  });
 
   $('#videoModal').on('hidden.bs.modal',function(e) {
     $('#videoModal iframe').attr('src', "");
     $('.modal-title').empty('value');
     $("#message_result_modal").hide();
-    $("#song_title").val("");
-    $("#song_videoId").val("")
-  })
+    $("#song_title, #song_videoId").val("");
+  });
 
   $('.playlist_created').on('change', function(){
     $.ajax({
-        url: 'playlists/' + $(this).val(),
-        type: 'GET',
-        success: function(result){
-        $("#js-playlist-content").html(result)
-      }
+      url: 'playlists/' + $(this).val(),
+      type: 'GET',
+      success: function(result){ $("#js-playlist-content").html(result) }
     })
+  });
 
-    $('#song_playlist_id').on('change', function(){
-      var currentPlaylistId = $(this).val();
-      if( currentPlaylistId.length > 0) {
-        $('#add_song_to_playlist_form').attr('action', '/playlists/' + $(this).val() + '/songs');
-        $('#add_song_to_playlist_form input[type=submit]').fadeIn('fast');
-      } else {
-        $('#add_song_to_playlist_form input[type=submit]').fadeOut('fast');
-      }
-    });
+  $('#song_playlist_id').on('change', function(){
+    var currentPlaylistId = $(this).val();
+    if( currentPlaylistId.length > 0) {
+      $('#add_song_to_playlist_form').attr('action', '/playlists/' + $(this).val() + '/songs');
+      $('#add_song_to_playlist_form input[type=submit]').fadeIn('fast');
+    } else {
+      $('#add_song_to_playlist_form input[type=submit]').fadeOut('fast');
+    }
   });
 });
