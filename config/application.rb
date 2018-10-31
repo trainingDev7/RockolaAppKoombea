3c3dd3768b325d2740021d376b931984b0c6c925
Felipe Llerena <felipe.llerena@koombea.com>
2018-10-31 15:05:44 -0500

    Eager load and action messages

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module RockolaAppKoombea
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.autoload_paths += %W( lib/ )
+    config.eager_load_paths << Rails.root.join('lib')
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
