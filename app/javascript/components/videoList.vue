dba2a77fbb4524772986f43c1d57ff9386990139
Felipe Llerena <felipe.llerena@koombea.com>
2018-11-20 15:39:32 -0500

    Adding modal component and socket notificacion

---
+++ app/javascript/components/videoList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card" style="width: 18rem;">
+    <img :src="video.snippet.thumbnails.medium.url" 
+    class="card-img-top" 
+    @click="modalShow(video)">
+    
+    <div class="card-body">
+      <h5 class="card-title">{{ video.snippet.title }}</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+import myModal from '../components/my-modal.vue'
+export default {
+  name: 'VideoList',
+  props: ['video'],
+  components: {
+    myModal
+  },
+  methods: {
+    modalShow(video){
+      this.$root.showModal(video)
+    }
+  }
+}
+</script>
+
